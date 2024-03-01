1b9392b5a676a7efed0fe378ac0d15f751b088bb
hua yang <360892334@qq.com>
2024-03-02 12:37:46 +1300

    c

--- biosecure-flask-project/connect.py
+++ biosecure-flask-project/connect.py
@@ -1,4 +1,3 @@
-# These details are available on the first MySQL Workbench screen
 # Usually called 'Local Instance'
 dbuser = "root" # Your MySQL username - likely 'root'
 dbpass = "1234" # ---- PUT YOUR PASSWORD HERE ----
