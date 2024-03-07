1d8bf4d0af2464b21b5f0d5daa933efb50511738
hua yang <360892334@qq.com>
2024-03-08 09:35:47 +1300

    delete

--- biosecure-flask-project/app/mariners/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-mariners_blueprint = Blueprint('mariners', __name__)
-
-from . import views
--- biosecure-flask-project/connect.py
+++ biosecure-flask-project/connect.py
@@ -1,6 +1,5 @@
-# Usually called 'Local Instance'
-dbuser = "root" # Your MySQL username - likely 'root'
-dbpass = "1234" # ---- PUT YOUR PASSWORD HERE ----
+dbuser = "root"  
+dbpass = "1234"  
 dbhost = "localhost" 
 dbport = "3306"
 dbname = "biosecurity"
