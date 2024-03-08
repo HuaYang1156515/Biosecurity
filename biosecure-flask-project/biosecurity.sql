300de23d9ceb2e34144539d511a234b6360e3b36
hua yang <360892334@qq.com>
2024-03-08 18:23:38 +1300

    Update biosecurity.sql

--- biosecure-flask-project/biosecurity.sql
+++ biosecure-flask-project/biosecurity.sql
@@ -1,5 +1,4 @@
-CREATE TABLE IF NOT EXISTS roles (
-    role_id INT AUTO_INCREMENT PRIMARY KEY,
-    role_name ENUM('mariner', 'staff', 'admin') UNIQUE NOT NULL,
-    description TEXT
-);
+INSERT INTO users (username, password, first_name, last_name, email, phone_number, date_joined, status, role) VALUES
+('mariner1', '{hashed_password}', 'John', 'Doe', 'johndoe@example.com', '123456789', CURDATE(), 'active', 'mariner'),
+('staff1', '{hashed_password}', 'Jane', 'Smith', 'janesmith@example.com', '987654321', CURDATE(), 'active', 'staff'),
+('admin', '{hashed_password}', 'Admin', 'User', 'adminuser@example.com', '192837465', CURDATE(), 'active', 'admin');
