0dc252d6e51544582544452521119d04a08e7832
hua yang <360892334@qq.com>
2024-03-13 00:27:13 +1300

    change

--- biosecure-flask-project/biosecurity.sql
+++ biosecure-flask-project/biosecurity.sql
@@ -1,4 +1,1 @@
-INSERT INTO users (username, password, first_name, last_name, email, phone_number, date_joined, status, role) VALUES
-('mariner1', '{hashed_password}', 'John', 'Doe', 'johndoe@example.com', '123456789', CURDATE(), 'active', 'mariner'),
-('staff1', '{hashed_password}', 'Jane', 'Smith', 'janesmith@example.com', '987654321', CURDATE(), 'active', 'staff'),
-('admin', '{hashed_password}', 'Admin', 'User', 'adminuser@example.com', '192837465', CURDATE(), 'active', 'admin');
+SELECT * FROM biosecurity.ocean_guide;
